1769b47bf4f39823562bbedfb8dae49137e167c3
truongan106 <truongan106@gmail.com>
2017-07-09 13:15:26 +0700

    a

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     console.log( "document loaded" );
     $(".container .welcome").text("Rất vui khi bạn ghé thăm nhà của An!").addClass("animated bounceIn");
     setTimeout(function(){
-    	$(".liveshow").text("na aaa aaa?").addClass("liveshow-button animated shake")
+    	$(".liveshow").text("Có gì vui?").addClass("liveshow-button animated shake q01")
     	whatfun();
     },3000);
 
@@ -10,16 +10,35 @@ $( document ).ready(function() {
 });
 
 var whatfun = function(){
-	$(".liveshow.liveshow-button.animated.shake").on("click",function(){
+	$(".q01").on("click",function(e){
+		$(e.currentTarget).remove();
 		question01();
-    })
+    });
 
 };
 
 var question01 =function() {
 	$(".container .welcome").text("Bạn có biết An hông?");
-	$(".liveshow").html('<div class="answer-yes-button">Biết chứ!</div><div class="answer-no-button">Mày là ai?</div>')
-	$(".answer-yes-button").addClass("bounce animated");
-	$(".answer-no-button").addClass("bounce animated");
+	$(".contents").html("").append('<div class="answer-yes-button">Biết chứ!</div><div class="answer-no-button">Mày là ai?</div>').removeClass("liveshow-button").css({
+		    display: "flex"
 
+	});
+  	$(".answer-yes-button").addClass("bounce animated").on("click",function(){
+  		q02(1);
+  	});
+	$(".answer-no-button").addClass("bounce animated").on("click",function(){
+  		 q02(2);
+
+  	});
+};
+
+var q02 =function(con) {
+	if(con==1){
+		$(".container .welcome").text("Từ 1 đến 10, An ");
+		$(".contents").html("").append('<div class="linkfb"><a href="https://www.facebook.com/truongan1006">https://www.facebook.com/truongan1006</a></div>');
+	
+	}else{
+		$(".container .welcome").text("Làm quen nhé! Đây là Facebook của An!");
+		$(".contents").html("").append('<div class="linkfb"><a href="https://www.facebook.com/truongan1006">https://www.facebook.com/truongan1006</a></div>');
+	}
 }
